ef73ea8cd43f00f94ea1d0422c75b1d321e2b77c
AlexanderC <alexander.moldova@gmail.com>
2014-11-11 18:12:44 +0200

    small plugin improvements

--- src/main/java/alexanderc/tweek/es/plugin/kas/KeyAwareSearchRestHandler.java
+++ src/main/java/alexanderc/tweek/es/plugin/kas/KeyAwareSearchRestHandler.java
@@ -7,9 +7,7 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.common.Strings;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.index.query.QueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.index.query.*;
 import org.elasticsearch.rest.*;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import static org.elasticsearch.rest.RestRequest.Method.GET;
@@ -25,7 +23,6 @@ public class KeyAwareSearchRestHandler extends BaseRestHandler {
     public static final String KEY_PARAM = "_key";
     public static final Integer DEFAULT_SIZE = 10;
     public static final String KEY_FIELD = "_kas_key";
-    public static final String DEFAULT_KEY = "_";
     public static final String ALL_INDEXES = "_all";
 
     @Inject
@@ -40,66 +37,45 @@ public class KeyAwareSearchRestHandler extends BaseRestHandler {
         String[] indices = Strings.splitStringByCommaToArray(restRequest.param("index", ALL_INDEXES));
         SearchRequest searchRequest = new SearchRequest(indices);
 
-        String _key = restRequest.param(KEY_PARAM, DEFAULT_KEY);
+        String _key = restRequest.param(KEY_PARAM, "");
         String query = restRequest.param(QUERY_PARAM, "");
         Integer from = restRequest.paramAsInt(FROM_PARAM, 0);
+        from = from < 0 ? 0 : from;
         Integer size = restRequest.paramAsInt(SIZE_PARAM, DEFAULT_SIZE);
+        size = size <= 0 ? DEFAULT_SIZE : size;
 
-        if(from < 0) {
-            from = 0;
+        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
+        sourceBuilder.fetchSource(null, Strings.addStringToArray(Strings.EMPTY_ARRAY, KEY_FIELD));
+        sourceBuilder.from(from);
+        sourceBuilder.size(size);
+
+        if(query.isEmpty()) {
+            sourceBuilder.query(QueryBuilders.termQuery(KEY_FIELD, _key));
+        } else {
+            sourceBuilder.query(QueryBuilders.queryString(query));
+            sourceBuilder.postFilter(FilterBuilders.termFilter(KEY_FIELD, _key));
         }
 
-        if(size <= 0) {
-            size = DEFAULT_SIZE;
-        }
+        searchRequest.extraSource(sourceBuilder);
 
-        // allow skipping the key parameter
-//        if(_key.isEmpty()) {
-//            RestError error = new RestError(
-//                    "You MUST provide the key as '%' request parameter".replace("%", KEY_PARAM),
-//                    RestStatus.BAD_REQUEST
-//            );
-//            restChannel.sendResponse(error);
-//        } else {
-            TermQueryBuilder keyQuery = QueryBuilders.termQuery(KEY_FIELD, _key);
+        // allow accessing from inner class
+        final Integer resultFrom = from;
+        final Integer resultSize = size;
 
-            QueryBuilder queryBuilder = query.isEmpty()
-                    ? QueryBuilders.matchAllQuery()
-                    : QueryBuilders.queryString(query);
-
-            SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-
-            String[] excludes = Strings.EMPTY_ARRAY;
-            excludes = Strings.addStringToArray(excludes, KEY_FIELD);
-
-            sourceBuilder.fetchSource(null, excludes);
-            sourceBuilder.from(from);
-            sourceBuilder.size(size);
-
-            sourceBuilder.query(queryBuilder);
-            //sourceBuilder.postFilter(keyQuery.toString());
-
-            searchRequest.extraSource(sourceBuilder);
-
-            // allow accessing from inner class
-            final Integer resultFrom = from;
-            final Integer resultSize = size;
-
-            client.search(searchRequest, new ActionListener<SearchResponse>() {
-                @Override
-                public void onResponse(SearchResponse searchResponse) {
-                    if(!searchResponse.status().equals(RestStatus.OK)) {
-                        restChannel.sendResponse(new RestError("Search failed.", RestStatus.INTERNAL_SERVER_ERROR));
-                    } else {
-                        restChannel.sendResponse(new ResultResponse(searchResponse, resultFrom, resultSize));
-                    }
+        client.search(searchRequest, new ActionListener<SearchResponse>() {
+            @Override
+            public void onResponse(SearchResponse searchResponse) {
+                if(!searchResponse.status().equals(RestStatus.OK)) {
+                    restChannel.sendResponse(new RestError("Search failed.", RestStatus.INTERNAL_SERVER_ERROR));
+                } else {
+                    restChannel.sendResponse(new ResultResponse(searchResponse, resultFrom, resultSize));
                 }
+            }
 
-                @Override
-                public void onFailure(Throwable throwable) {
-                    restChannel.sendResponse(new RestError(throwable.getMessage(), RestStatus.INTERNAL_SERVER_ERROR));
-                }
-            });
-//        }
+            @Override
+            public void onFailure(Throwable throwable) {
+                restChannel.sendResponse(new RestError(throwable.getMessage(), RestStatus.INTERNAL_SERVER_ERROR));
+            }
+        });
     }
 }
--- src/main/java/alexanderc/tweek/es/plugin/kas/ResultResponse.java
+++ src/main/java/alexanderc/tweek/es/plugin/kas/ResultResponse.java
@@ -12,26 +12,26 @@ import java.util.Iterator;
  */
 public class ResultResponse extends BaseResponse {
     public ResultResponse(final SearchResponse searchResponse, final Integer offset, final Integer limit) {
-//        Iterator<SearchHit> hits = searchResponse.getHits().iterator();
-//
-//        String[] collection = Strings.EMPTY_ARRAY;
-//
-//        while(hits.hasNext()) {
-//            collection = Strings.addStringToArray(collection, ((SearchHit) hits.next()).getSourceAsString());
-//        }
-//
-//        StringBuilder sb = new StringBuilder();
-//
-//        sb.append("{");
-//        sb.append("\"pagination\":{\"count\":%1,\"limit\":%2,\"offset\":%3},"
-//                        .replace("%1", Long.toString(searchResponse.getHits().getTotalHits()))
-//                        .replace("%2", Integer.toString(limit))
-//                        .replace("%3", Integer.toString(offset)));
-//        sb.append("\"results\":[");
-//        sb.append(Strings.arrayToCommaDelimitedString(collection));
-//        sb.append("]}");
+        Iterator<SearchHit> hits = searchResponse.getHits().iterator();
 
-        StringAndBytesText responseObject = new StringAndBytesText(/*sb.toString()*/searchResponse.toString());
+        String[] collection = Strings.EMPTY_ARRAY;
+
+        while(hits.hasNext()) {
+            collection = Strings.addStringToArray(collection, ((SearchHit) hits.next()).getSourceAsString());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{");
+        sb.append("\"pagination\":{\"count\":%1,\"limit\":%2,\"offset\":%3},"
+                        .replace("%1", Long.toString(searchResponse.getHits().getTotalHits()))
+                        .replace("%2", Integer.toString(limit))
+                        .replace("%3", Integer.toString(offset)));
+        sb.append("\"results\":[");
+        sb.append(Strings.arrayToCommaDelimitedString(collection));
+        sb.append("]}");
+
+        StringAndBytesText responseObject = new StringAndBytesText(sb.toString());
 
         this.setBytes(responseObject.bytes());
         this.setStatus(RestStatus.OK);
